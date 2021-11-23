ffe210beedd20d48295bc25d7801c5b4fc69ee27
Anthony Santos <anthonygabriel642@gmail.com>
2021-11-23 16:01:00 -0300

    Coreção de erros na class 'Produtos'

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -8,14 +8,14 @@ namespace ProdutosApi.Models
         [Key]
         public int Id { set; get; }
 
-        [Required(ErrorMessage = "Este campo é obrigatorio")]
+        [Required(ErrorMessage = "Este campo é obrigatorio", AllowEmptyStrings = false)]
         public string Nome { get; set; }
 
         [Required(ErrorMessage = "Este campo é obrigatorio")]
         [Range(1, int.MaxValue, ErrorMessage = "O preço deve ser maior que zero")]
-        public bool Valo_Unitario { set; get; }
+        public decimal Valor_Unitario { set; get; }
 
-        [Required(ErrorMessage = "Este campo é obrigatorio")]
+        [Required(ErrorMessage = "Este campo é obrigatorio", AllowEmptyStrings = false)]
         public int Qtde_Estoque { set; get; }
 
         public DateTime Ultima_Venda { set; get; }
